2ad2843e194967ed63a227d682ecf46441eaf5d1
sskim91 <tjdtjq91@gmail.com>
2021-07-26 21:50:41 +0900

    Feat: 조인

---
+++ src/main/java/com/kyh/hellojpa/JpaJoinMain.java
@@ -0,0 +1,57 @@
+package com.kyh.hellojpa;
+
+import com.kyh.hellojpa.domain.Member;
+import com.kyh.hellojpa.domain.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+/**
+ * Created by sskim on 2021/07/26
+ * Github : http://github.com/sskim91
+ */
+public class JpaJoinMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member");
+            member.setAge(10);
+            member.changeTeam(team);
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+//            String query = "select m from Member m inner join m.team t";
+//            String query = "select m from Member m left outer join m.team t";
+//            String query = "select m from Member m, Team t where m.username = t.name";
+//            String query = "select m from Member m left join m.team t on t.name = 'teamA'";
+            String query = "select m from Member m left join Team t on m.username = t.name";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.clear();
+        }
+        emf.close();
+    }
+}
